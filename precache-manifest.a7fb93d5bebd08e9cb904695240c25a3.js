455d14ab5f8b732fd363af5c11dd06d73f5887aa
Musa <ngcobomusa950@gmail.com>
2019-10-01 16:22:24 +0200

    Updates

---
+++ precache-manifest.a7fb93d5bebd08e9cb904695240c25a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fd27c7bbd83d6747fd88b6991720c3e",
+    "url": "/MusaWeather/index.html"
+  },
+  {
+    "revision": "e64e7787157e58811049",
+    "url": "/MusaWeather/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d2dac645b9d4707861a0",
+    "url": "/MusaWeather/static/css/main.f4a0146b.chunk.css"
+  },
+  {
+    "revision": "e64e7787157e58811049",
+    "url": "/MusaWeather/static/js/2.287d4f9b.chunk.js"
+  },
+  {
+    "revision": "d2dac645b9d4707861a0",
+    "url": "/MusaWeather/static/js/main.735f70c6.chunk.js"
+  },
+  {
+    "revision": "60f3f7b06357b7b1b457",
+    "url": "/MusaWeather/static/js/runtime~main.1124a676.js"
+  },
+  {
+    "revision": "e75e709c62575579df9f9d03fa3d6e2a",
+    "url": "/MusaWeather/static/media/weather.e75e709c.jpg"
+  }
+]);
